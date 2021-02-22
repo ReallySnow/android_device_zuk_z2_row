AyushR1/device_zuk_z2_plus

Changes for PixelExperience

AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_z2_plus.mk
+    $(LOCAL_DIR)/aosp_z2_plus.mk

ReallySnow/android_device_zuk_z2_row

AndroidProducts.mk
@@ -14,9 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_z2_row.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_z2_row-userdebug \
-    lineage_z2_row-user \
-    lineage_z2_row-eng
+    $(LOCAL_DIR)/aosp_z2_row.mk